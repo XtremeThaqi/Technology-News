fdc3ae45f14a2ed63dff9dda71f9aadb32c97622
XtremeThaqi <erblinthaqi40@gmail.com>
2024-10-15 22:26:25 +0200

    fixed home page and added some more
    added about articles index html   and index js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('openMenu').addEventListener('click', () => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+  });
